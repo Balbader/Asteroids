af27ef447d7af23f0c87c63900939e3bf992c29e
BASIL ALBADER <basilalbapro@gmail.com>
2025-06-10 19:52:59 +0200

    WIP

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()

b5cba7c3afcb9edaaff351d09ab82bc72b78f84f
BASIL ALBADER <basilalbapro@gmail.com>
2025-06-12 16:16:15 +0200

    WIP

--- asteroid.py
+++ asteroid.py
@@ -29,14 +29,11 @@ class Asteroid(CircleShape):
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
-        
         random_angle = random.uniform(20, 50)
         new_velocity1 = self.velocity.rotate(random_angle)
         new_velocity2 = self.velocity.rotate(-random_angle)
         new_radius = self.radius - ASTEROID_MIN_RADIUS
-        
         asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid1.velocity = new_velocity1 * 1.2
-        
         asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid2.velocity = new_velocity2 * 1.2
